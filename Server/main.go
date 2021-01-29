a26afd5ade797e7ccca20a97743873591d4c4412
liaocenchen <liaocenchen@ztgame.com>
2021-01-29 15:00:43 +0800

    提交服务器文件版1

---
+++ Server/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"./game"
+	"zero"
+)
+
+func main() {
+	host := ":18787"
+
+	ss, err := zero.NewSocketService(host)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	ss.SetHeartBeat(5*time.Second, 30*time.Second)
+
+	ss.RegMessageHandler(game.HandleMessage)
+	ss.RegConnectHandler(game.HandleConnect)
+	ss.RegDisconnectHandler(game.HandleDisconnect)
+
+	log.Println("server running on " + host)
+	ss.Serv()
+}
+
